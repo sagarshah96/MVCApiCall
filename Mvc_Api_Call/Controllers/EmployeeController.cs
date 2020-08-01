364ccd4ade18a9dd08d678de154c1fcf23f78ed3
Sagar <Sagar@SAGAR>
2020-08-01 21:41:17 +0530

    Add new Employee controller,model

---
+++ Mvc_Api_Call/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Mvc_Api_Call.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Api_Call.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<EmpModelCont> empobj = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:50691/api/Employee/GetEmployee");
+            var consumeapi = hc.GetAsync("GetEmployee");
+            consumeapi.Wait();
+            var readData = consumeapi.Result;
+            if (readData.IsSuccessStatusCode)
+            {
+                var displayData = readData.Content.ReadAsAsync<IList<EmpModelCont>>();
+                displayData.Wait();
+                empobj = displayData.Result;
+            }
+            return View(empobj);
+        }
+    }
+}
---
+++ Mvc_Api_Call/Models/EmpModelCont.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_Api_Call.Models
+{
+    public class EmpModelCont
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+        public string Designation { get; set; }
+    }
+}
