777ec11a76d86ef4cc3a4774ad525c30b791a8e9
Sagar <Sagar@SAGAR>
2020-08-01 21:31:52 +0530

    Add project files.

---
+++ Mvc_Api_Call/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_Api_Call.Startup))]
+namespace Mvc_Api_Call
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
